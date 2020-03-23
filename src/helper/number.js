73e6a4ad2d1c456cedc6a42bb527c6d3e478722d
Dmitriy Naumenko <naymwm@gmail.com>
2020-03-23 12:25:04 +0200

    fix(helpers): convert string to number

--- src/helper/number.js
+++ src/helper/number.js
@@ -7,11 +7,17 @@
 export function toNumber(number) {
   let result;
 
-  if (typeof number === 'number') {
+  if (typeof number === "number") {
     result = number;
-
-  } else if (typeof number === 'string') {
-    result = number.indexOf('.') > -1 ? parseFloat(number) : parseInt(number, 10);
+  } else if (typeof number === "string") {
+    if (number.trim() === "") {
+      result = 0;
+    } else {
+      result =
+        number.indexOf(".") > -1 ? parseFloat(number) : parseInt(number, 10);
+    }
+  } else if (number === null || number === undefined) {
+    result = 0;
   }
 
   return result;
