3afd5c1eb1a73f51bf8a4baa5affa51fffd95c01
Dmitriy Naumenko <naymwm@gmail.com>
2020-03-24 10:56:03 +0200

    add es code

---
+++ es/helper/number.js
@@ -0,0 +1,33 @@
+/**
+ * Convert value into number.
+ *
+ * @param {String|Number} number
+ * @returns {*}
+ */
+export function toNumber(number) {
+  var result = void 0;
+
+  if (typeof number === "number") {
+    result = number;
+  } else if (typeof number === "string") {
+    if (number.trim() === "") {
+      result = 0;
+    } else {
+      result = number.indexOf(".") > -1 ? parseFloat(number) : parseInt(number, 10);
+    }
+  } else if (number === null || number === undefined) {
+    result = 0;
+  }
+
+  return result;
+}
+
+/**
+ * Invert provided number.
+ *
+ * @param {Number} number
+ * @returns {Number} Returns inverted number.
+ */
+export function invertNumber(number) {
+  return -1 * toNumber(number);
+}
